ad4e5d6f2fe0a2456c8042bc68d62ff5e9d2bcba
unknown <ujjwalg056@gmail.com>
2024-07-12 20:52:00 +0530

    gross profit and net profit

--- pages/beast.tsx
+++ pages/beast.tsx
@@ -1,9 +1,8 @@
-// /pages/beast.tsx
-
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Transition } from '@headlessui/react';
 import axios from 'axios';
+import { Switch } from '@nextui-org/react';
 
 const fetchTradingHistory = async (from: number, to: number, marketId: string | null, setTradingData: Function) => {
     try {
@@ -26,11 +25,17 @@ const copyToClipboard = (text: string, setNotificationVisible: Function) => {
     );
 };
 
+const formatAddress = (address: string) => {
+    return `${address.slice(0, 5)}...${address.slice(-3)}`;
+};
+
 const BeastPage = () => {
     const router = useRouter();
     const { from, to, marketId } = router.query;
-    const [tradingData, setTradingData] = useState<Record<string, number> | null>(null);
+    const [tradingData, setTradingData] = useState<any[] | null>(null);
     const [notificationVisible, setNotificationVisible] = useState(false);
+    const [sortGrossDescending, setSortGrossDescending] = useState(false);
+    const [sortNetDescending, setSortNetDescending] = useState(true); // Initially set to false to prioritize Gross Profit sorting by default
 
     useEffect(() => {
         if (from && to) {
@@ -41,39 +46,104 @@ const BeastPage = () => {
         }
     }, [from, to, marketId]);
 
+    const sortedData = tradingData
+        ? tradingData.sort((a, b) => {
+            if (sortGrossDescending) {
+                return parseFloat(b["gross profit"]) - parseFloat(a["gross profit"]);
+            } else if (sortNetDescending) {
+                return parseFloat(b["net profit"]) - parseFloat(a["net profit"]);
+            }
+            return 0;
+        })
+        : [];
+
+    const handleGrossSwitchChange = () => {
+        setSortGrossDescending(!sortGrossDescending);
+        setSortNetDescending(false);
+        console.log(`Gross Profit sorting is now ${!sortGrossDescending ? "Descending" : "Ascending"}`);
+    };
+
+    const handleNetSwitchChange = () => {
+        setSortNetDescending(!sortNetDescending);
+        setSortGrossDescending(false);
+        console.log(`Net Profit sorting is now ${!sortNetDescending ? "Descending" : "Ascending"}`);
+    };
 
     return (
         <div className="relative flex flex-col items-center gap-6 p-6 bg-gray-100 min-h-screen">
-            <h1 className="text-2xl font-bold text-blue-800">Trading History</h1>
+            <div className="flex justify-between w-full max-w-2xl">
+                <h1 className="text-2xl font-bold text-blue-800">Trading History</h1>
+                <div className="flex gap-4">
+                    <Switch
+                        defaultSelected={sortGrossDescending}
+                        size="lg"
+                        color="secondary"
+                        onChange={handleGrossSwitchChange}
+                        thumbIcon={({ isSelected, className }) =>
+                            isSelected ? (
+                                <p>⬇️</p> 
+                            ) : (
+                                <p>⬆️</p>
+                            )
+                        }
+                    >
+                        Gross Profit {sortGrossDescending ? "Descending" : "Ascending"}
+                    </Switch>
+                    <Switch
+                        defaultSelected={sortNetDescending}
+                        size="lg"
+                        color="secondary"
+                        onChange={handleNetSwitchChange}
+                        thumbIcon={({ isSelected, className }) =>
+                            isSelected ? (
+                                <p>⬇️</p> 
+                            ) : (
+                                <p>⬆️</p>
+                            )
+                        }
+                    >
+                        Net Profit {sortNetDescending ? "Descending" : "Ascending"}
+                    </Switch>
+                </div>
+            </div>
 
             {tradingData ? (
-                <div className="mt-6 w-full max-w-2xl bg-white/30 backdrop-blur-md rounded-xl shadow-lg overflow-hidden">
-                    <table className="min-w-full bg-white/60 rounded-md backdrop-blur-md">
-                        <thead>
-                            <tr>
-                                <th className="px-6 py-3 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">
-                                    USER
-                                </th>
-                                <th className="px-6 py-3 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">
-                                    PNL USD
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Object.entries(tradingData).map(([user, pnl]) => (
-                                <tr key={user}>
-                                    <td className="px-6 py-4 border-b border-blue-300 text-blue-800 text-lg cursor-pointer" onClick={() => copyToClipboard(user, setNotificationVisible)}>
-                                        {user}
-                                    </td>
-                                    <td className="px-6 py-4 border-b border-blue-300 text-blue-800 text-lg">
-                                        {pnl.toFixed(2)}
-                                    </td>
+                <div className="mt-6 w-full max-w-2xl mx-auto">
+                    <p className="text-sm text-gray-600 mb-2 text-left">PNL now includes the entry fee paid by the trader.</p>
+                    <div className="bg-white/30 backdrop-blur-md rounded-xl shadow-lg overflow-hidden">
+                        <table className="min-w-full w-auto bg-white/60 rounded-md backdrop-blur-md">
+                            <thead>
+                                <tr>
+                                    <th className="px-4 py-2 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">
+                                        USER
+                                    </th>
+                                    <th className="px-4 py-2 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">
+                                        GROSS PROFIT
+                                    </th>
+                                    <th className="px-4 py-2 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">
+                                        NET PROFIT
+                                    </th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {sortedData.map(({ owner, "gross profit": grossProfit, "net profit": netProfit }) => (
+                                    <tr key={owner}>
+                                        <td className="px-4 py-2 border-b border-blue-300 text-blue-800 text-lg cursor-pointer" onClick={() => copyToClipboard(owner, setNotificationVisible)}>
+                                            {formatAddress(owner)}
+                                        </td>
+                                        <td className="px-4 py-2 border-b border-blue-300 text-blue-800 text-lg">
+                                            {grossProfit}
+                                        </td>
+                                        <td className="px-4 py-2 border-b border-blue-300 text-blue-800 text-lg">
+                                            {netProfit}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            ) :(
+            ) : (
                 <div className="flex items-center justify-center flex-grow">
                     <svg className="animate-spin h-12 w-12 text-blue-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle
@@ -91,8 +161,7 @@ const BeastPage = () => {
                         ></path>
                     </svg>
                 </div>
-            )
-            }
+            )}
 
             <Transition
                 show={notificationVisible}

32312ee409ebe15178f399c17a0a4f1a2b120353
unknown <ujjwalg056@gmail.com>
2024-08-02 15:54:12 +0530

    updated trades CSV with timestamp to UTC and fixed inconsistency with all trades and trader specific data

--- pages/beast.tsx
+++ pages/beast.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import FileSaver from "file-saver";
 import { Parser } from "json2csv";
 import { PoolConfig, Token } from "flash-sdk";
-import { marketInfo, MarketInfo } from "@/utils/marketInfo";
+import { marketInfo } from "@/utils/marketInfo";
 
 interface Profit {
   "net profit": number;
@@ -33,35 +33,24 @@ interface Trade {
   id: number;
   oraclePrice: string;
   oraclePriceExponent: number;
-}
+};
+
+type UsdFields = 'sizeUsd' | 'collateralUsd' | 'pnlUsd';
+
 
 const POOL_NAMES = ["Crypto.1", "Virtual.1", "Governance.1", "Community.1"];
 const POOL_CONFIGS = POOL_NAMES.map((f) =>
   PoolConfig.fromIdsByName(f, "mainnet-beta")
 );
 
-// Define DUPLICATE_TOKENS and tokenMap
-const DUPLICATE_TOKENS = POOL_CONFIGS.map((f) => f.tokens).flat();
-const tokenMap = new Map();
-for (const token of DUPLICATE_TOKENS) {
-  tokenMap.set(token.symbol, token);
-}
-
-// Define ALL_TOKENS
+const DUPLICATE_TOKENS = POOL_CONFIGS.flatMap((f) => f.tokens);
+const tokenMap = new Map(DUPLICATE_TOKENS.map((token) => [token.symbol, token]));
 const ALL_TOKENS: Token[] = Array.from(tokenMap.values());
 
-const fetchTradingHistory = async (
-  from: number,
-  to: number,
-  marketId: string | null,
-  setTradingData: Function
-) => {
+const fetchTradingHistory = async (from: number, to: number, marketId: string | null, setTradingData: Function) => {
   try {
-    const response = await axios.get(
-      `/api/getPnl?from=${from}&to=${to}&marketId=${marketId}`
-    );
+    const response = await axios.get(`/api/getPnl?from=${from}&to=${to}&marketId=${marketId}`);
     setTradingData(response.data);
-    console.log("Trading history data:", response.data);
   } catch (error) {
     console.error("Error fetching trading history:", error);
   }
@@ -69,9 +58,7 @@ const fetchTradingHistory = async (
 
 const fetchUserTradingHistory = async (address: string) => {
   try {
-    const response = await axios.get(
-      `https://api.prod.flash.trade/trading-history/find-all-by-user-v2/${address}`
-    );
+    const response = await axios.get(`https://api.prod.flash.trade/trading-history/find-all-by-user-v2/${address}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching user trading history:", error);
@@ -79,117 +66,87 @@ const fetchUserTradingHistory = async (address: string) => {
   }
 };
 
-const getTraderCsv = async (address: string) => {
-  const tradingHistory = await fetchUserTradingHistory(address);
-  if (tradingHistory.length === 0) return;
+const formatTradeData = (trades: Trade[]): Trade[] => {
+  return trades.map((trade) => {
+    const marketKey = trade.market;
+    if (marketInfo[marketKey]) {
+      trade.market = marketInfo[marketKey].tokenPair;
+    }
 
+    (['sizeUsd', 'collateralUsd', 'pnlUsd'] as UsdFields[]).forEach((field) => {
+      if (trade[field]) {
+        trade[field] = (parseFloat(trade[field] as string) * 10 ** -6).toString();
+      }
+    });
+
+    const collateralToken = ALL_TOKENS.find((token) => token.symbol === trade.market);
+    const collateralTokenDecimals = collateralToken?.decimals || 0;
+    const collateralTokenPrice =
+      trade.side === 'long'
+        ? trade.oraclePrice
+          ? parseFloat(trade.oraclePrice) * 10 ** trade.oraclePriceExponent
+          : trade.price
+          ? parseFloat(trade.price) * 10 ** -6
+          : 1.0
+        : 1.0;
+    trade.oraclePrice = collateralTokenPrice.toString();
+
+    let feesUsd;
+    if (trade.feeAmount) {
+      feesUsd = parseFloat(trade.feeAmount) * 10 ** -collateralTokenDecimals * collateralTokenPrice;
+    }
+
+    // Convert the timestamp to human-readable UTC standard date and time
+    const timestamp = parseInt(trade.timestamp, 10);
+    const date = new Date(timestamp * 1000).toISOString();
+
+    return { ...trade, feesUsd, timestamp: date };
+  });
+};
+
+
+
+
+const generateCSV = (data: Trade[], filename: string) => {
   const fields = [
-    "txId",
-    "eventIndex",
-    "timestamp",
-    "positionAddress",
-    "owner",
-    "market",
-    "side",
-    "tradeType",
-    "price",
-    "sizeUsd",
-    "sizeAmount",
-    "collateralUsd",
-    "collateralPrice",
-    "collateralAmount",
-    "pnlUsd",
-    "liquidationPrice",
-    "feeAmount",
-    "id",
-    "oraclePrice",
-    "oraclePriceExponent",
+    "txId", "eventIndex", "timestamp", "positionAddress", "owner", "market",
+    "side", "tradeType", "price", "sizeUsd", "sizeAmount", "collateralUsd",
+    "collateralPrice", "collateralAmount", "pnlUsd", "liquidationPrice",
+    "feeAmount", "feesUsd", "id", "oraclePrice", "oraclePriceExponent"
   ];
-
   const json2csvParser = new Parser({ fields });
+
   try {
-    const csv = json2csvParser.parse(tradingHistory);
+    const csv = json2csvParser.parse(data);
     const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
-    FileSaver.saveAs(blob, `trading_history_${address}.csv`);
+    FileSaver.saveAs(blob, filename);
   } catch (error) {
     console.error("Error converting JSON to CSV:", error);
   }
 };
 
+const getTraderCsv = async (address: string) => {
+  const filteredData = await fetchUserTradingHistory(address);
+  if (filteredData.length === 0) return;
+
+  const updatedData = formatTradeData(filteredData);
+  generateCSV(updatedData, `trading_history_${address}.csv`);
+};
+
 const fetchAllSettledPosition = async (from: number, to: number, marketId?: string, setLoading?: Function) => {
   if (setLoading) setLoading(true);
   try {
-    // &eventTypes=
     const queryParams = `to=${to}&from=${from}&eventTypes=OPEN_POSITION&eventTypes=CLOSE_POSITION&eventTypes=TAKE_PROFIT&eventTypes=STOP_LOSS&eventTypes=LIQUIDATE&eventTypes=ADD_COLLATERAL&eventTypes=REMOVE_COLLATERAL&eventTypes=INCREASE_SIZE&eventTypes=DECREASE_SIZE`;
     const url = `https://api.prod.flash.trade/trading-history/filter?${queryParams}`;
     const response = await axios.get(url);
 
     let filteredData = response.data;
-
     if (marketId) {
-      filteredData = filteredData.filter((trade: any) => trade.market === marketId);
+      filteredData = filteredData.filter((trade:Trade) => trade.market === marketId);
     }
 
-    // Update the market field with tokenPair from marketInfo and calculate feesUsd
-    const updatedData = filteredData.map((trade: Trade) => {
-      const marketKey = trade.market;
-      if (marketInfo[marketKey]) {
-        trade.market = marketInfo[marketKey].tokenPair;
-      }
-      if (trade.sizeUsd) {
-        trade.sizeUsd = (parseFloat(trade.sizeUsd) * 10 **-6).toString()
-      }
-      if (trade.collateralUsd) {
-        trade.collateralUsd = (parseFloat(trade.collateralUsd) * 10 **-6).toString()
-      }
-      if (trade.pnlUsd) {
-        trade.pnlUsd = (parseFloat(trade.pnlUsd) * 10 **-6).toString()
-      }
-      const collateralTokenDecimals = ALL_TOKENS.find((i) => i.symbol === trade.market)?.decimals || 0;
-      const collateralTokenPrice = trade.side === "long"
-        ? (trade.oraclePrice !== null ? parseFloat(trade.oraclePrice) * 10 ** trade.oraclePriceExponent : (trade.price !== null ? parseFloat(trade.price) * 10 ** -6 : 1.0))
-        : 1.0;
-      trade.oraclePrice = collateralTokenPrice.toString();
-
-      let feesUsd;
-      if (trade.feeAmount) {
-        feesUsd = parseFloat(trade.feeAmount) * (10 ** -collateralTokenDecimals) * collateralTokenPrice;
-      }
-      return { ...trade, feesUsd };
-    });
-
-    const fields = [
-      "txId",
-      "eventIndex",
-      "timestamp",
-      "positionAddress",
-      "owner",
-      "market", // This will now contain the tokenPair value
-      "side",
-      "tradeType",
-      "price",
-      "sizeUsd",
-      "sizeAmount",
-      "collateralUsd",
-      "collateralPrice",
-      "collateralAmount",
-      "pnlUsd",
-      "liquidationPrice",
-      "feeAmount",
-      "feesUsd", // New field
-      "id",
-      "oraclePrice",
-      "oraclePriceExponent",
-    ];
-
-    const json2csvParser = new Parser({ fields });
-try {
-      const csv = json2csvParser.parse(updatedData);
-      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
-      FileSaver.saveAs(blob, `settled_positions_${from}_to_${to}.csv`);
-    } catch (error) {
-      console.error("Error converting JSON to CSV:", error);
-    }
+    const updatedData = formatTradeData(filteredData);
+    generateCSV(updatedData, `settled_positions_${from}_to_${to}.csv`);
   } catch (error) {
     console.error("Error fetching settled positions:", error);
   } finally {
@@ -197,13 +154,11 @@ try {
   }
 };
 
-
 const copyToClipboard = (text: string, setNotificationVisible: Function) => {
   navigator.clipboard.writeText(`https://beast.flash.trade/?public_key=${text}`).then(
     () => {
-      console.log(`Copied https://beast.flash.trade/?public_key=${text} to clipboard`);
       setNotificationVisible(true);
-      setTimeout(() => setNotificationVisible(false), 2000); // Hide after 2 seconds
+      setTimeout(() => setNotificationVisible(false), 2000);
     },
     (err) => console.error("Could not copy text to clipboard", err)
   );
@@ -226,12 +181,7 @@ const BeastPage = () => {
       const fromTimestamp = parseInt(from as string, 10);
       const toTimestamp = parseInt(to as string, 10);
       const validMarketId = typeof marketId === "string" ? marketId : null;
-      fetchTradingHistory(
-        fromTimestamp,
-        toTimestamp,
-        validMarketId,
-        setTradingData
-      );
+      fetchTradingHistory(fromTimestamp, toTimestamp, validMarketId, setTradingData);
     }
   }, [from, to, marketId]);
 
@@ -259,18 +209,10 @@ const BeastPage = () => {
           <table className="min-w-full bg-white/60 rounded-md backdrop-blur-md">
             <thead>
               <tr>
-                <th className="px-6 py-3 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">
-                  Trader
-                </th>
-                <th className="px-6 py-3 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">
-                  Net Profit
-                </th>
-                <th className="px-6 py-3 border-b-2 border-blue-300 text-center text-lg font-medium text-blue-800">
-                  Gross Profit
-                </th>
-                <th className="px-6 py-3 border-b-2 border-blue-300 text-center text-lg font-medium text-blue-800">
-                  CSV Of Trades
-                </th>
+                <th className="px-6 py-3 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">Trader</th>
+                <th className="px-6 py-3 border-b-2 border-blue-300 text-left text-lg font-medium text-blue-800">Net Profit</th>
+                <th className="px-6 py-3 border-b-2 border-blue-300 text-center text-lg font-medium text-blue-800">Gross Profit</th>
+                <th className="px-6 py-3 border-b-2 border-blue-300 text-center text-lg font-medium text-blue-800">CSV Of Trades</th>
               </tr>
             </thead>
             <tbody>
@@ -278,25 +220,14 @@ const BeastPage = () => {
                 <tr key={user}>
                   <td
                     className="px-6 py-4 border-b border-blue-300 text-blue-800 text-lg cursor-pointer"
-                    onClick={() =>
-                      copyToClipboard(user, setNotificationVisible)
-                    }
+                    onClick={() => copyToClipboard(user, setNotificationVisible)}
                   >
                     {shortenAddress(user)}
                   </td>
-                  <td className="px-6 py-4 border-b border-blue-300 text-blue-800 text-lg">
-                    {pnl["net profit"].toFixed(2)}
-                  </td>
-                  <td className="px-6 py-4 border-b border-blue-300 text-blue-800 text-lg">
-                    {pnl["gross profit"].toFixed(2)}
-                  </td>
+                  <td className="px-6 py-4 border-b border-blue-300 text-blue-800 text-lg">{pnl["net profit"].toFixed(2)}</td>
+                  <td className="px-6 py-4 border-b border-blue-300 text-blue-800 text-lg">{pnl["gross profit"].toFixed(2)}</td>
                   <td className="px-6 py-4 border-b border-blue-300 text-center">
-                    <button
-                      onClick={() => getTraderCsv(user)}
-                      className="ml-4 p-1 bg-blue-600 text-white text-sm rounded-md hover:bg-blue-700"
-                    >
-                      ⬇️
-                    </button>
+                    <button onClick={() => getTraderCsv(user)} className="ml-4 p-1 bg-blue-600 text-white text-sm rounded-md hover:bg-blue-700">⬇️</button>
                   </td>
                 </tr>
               ))}
@@ -305,25 +236,9 @@ const BeastPage = () => {
         </div>
       ) : (
         <div className="flex items-center justify-center flex-grow">
-          <svg
-            className="animate-spin h-12 w-12 text-blue-800"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-            ></path>
+          <svg className="animate-spin h-12 w-12 text-blue-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
           </svg>
         </div>
       )}
